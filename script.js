bafa16e020c476d1c9cfbb2364a4fae814eca502
Muhammad Riziq Zhapran <134038828+nicwaproject@users.noreply.github.com>
2024-09-21 21:21:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 document.getElementById('order-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form from submitting the traditional way
+  event.preventDefault(); // Prevent form from submitting the traditional way
+
+  // Retrieve form data
+  var name = document.getElementById('name').value;
+  var theme = document.getElementById('theme').value;
+  var package = document.getElementById('package').value;
+
+  // Format the message
+  var message = `Hello, nama saya ${name}. Aku ingin memesan undangan dengan:\n- Tema: ${theme}\n- Paket: ${package}`;
+
+  // WhatsApp Business number (include country code, e.g., +6281234567890)
+  var phoneNumber = '+6285974176110';
   
-    // Retrieve form data
-    var name = document.getElementById('name').value;
-    var product = document.getElementById('product').value;
-  
-    // Format the message
-    var message = `Hello, my name is ${name}. I would like to order the theme: ${product}.`;
-  
-    // WhatsApp Business number (include country code, e.g., +6281234567890)
-    var phoneNumber = '+6285974176110';
-    
-    // Create WhatsApp URL
-    var whatsappUrl = `https://api.whatsapp.com/send/?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
-  
-    // Open WhatsApp with the formatted message
-    window.open(whatsappUrl, '_blank');
-  });
+  // Create WhatsApp URL
+  var whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+
+  // Open WhatsApp with the formatted message
+  window.open(whatsappUrl, '_blank');
+});
     
 // Function to toggle between light and dark mode
 function toggleTheme() {
